3b02af0ec81ab1d3e258e05070e3fc549af3e7cf
Melon006 <88492848+Melon006@users.noreply.github.com>
2026-02-23 10:31:46 +0530

    Add Flask app with password checking and port scanning

--- app.py
+++ app.py
@@ -1,1 +1,37 @@
+from flask import Flask,render_template,request
+from password_checker import check_password
+from port_scanner import scan_ports
 
+app = Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def home():
+
+    strength=None
+    entropy=None
+    score=None
+    crack=None
+    color=None
+    ports=None
+
+    if request.method=="POST":
+
+        if "password" in request.form:
+            strength,entropy,score,crack,color=\
+            check_password(request.form["password"])
+
+        if "target" in request.form:
+            ports=scan_ports(request.form["target"])
+
+    return render_template(
+        "index.html",
+        strength=strength,
+        entropy=entropy,
+        score=score,
+        crack=crack,
+        color=color,
+        ports=ports
+    )
+
+if __name__=="__main__":
+    app.run(debug=True)
