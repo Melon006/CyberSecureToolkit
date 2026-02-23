2bc4f31718229cd643ad87c7eb62df80f5f44c54
Melon006 <88492848+Melon006@users.noreply.github.com>
2026-02-23 10:52:35 +0530

    Refactor port scanner to use COMMON_PORTS and resolve target
    Updated common services to common ports and added target resolution function.

--- port_scanner.py
+++ port_scanner.py
@@ -1,28 +1,48 @@
 import socket
 
-COMMON_SERVICES = {
+COMMON_PORTS = {
     21:"FTP",
     22:"SSH",
+    23:"Telnet",
+    25:"SMTP",
+    53:"DNS",
     80:"HTTP",
+    110:"POP3",
+    139:"NetBIOS",
+    143:"IMAP",
     443:"HTTPS",
-    3306:"MySQL"
+    445:"SMB",
+    3306:"MySQL",
+    8080:"HTTP-Proxy"
 }
+
+def resolve_target(target):
+    try:
+        return socket.gethostbyname(target)
+    except:
+        return None
+
 
 def scan_ports(target):
 
+    ip = resolve_target(target)
+    if not ip:
+        return None, []
+
     results=[]
 
-    for port,service in COMMON_SERVICES.items():
+    for port,service in COMMON_PORTS.items():
 
-        s=socket.socket()
-        s.settimeout(0.5)
+        s=socket.socket(socket.AF_INET,
+                        socket.SOCK_STREAM)
+        s.settimeout(0.4)
 
         try:
-            s.connect((target,port))
+            s.connect((ip,port))
             results.append((port,service))
         except:
             pass
 
         s.close()
 
-    return results
+    return ip, results
